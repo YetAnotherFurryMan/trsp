20376cf9ec6bf7d34751f54368e8acbc77a611c9
YetAnotherFurryMan <damiansatala@gmail.com>
2024-05-27 13:26:47 +0200

    Better str.copy

--- src/common/str.zig
+++ src/common/str.zig
@@ -1,9 +1,7 @@
 const std = @import("std");
 const mem = std.mem;
 
-pub inline fn copy(str: []const u8, allocator: mem.Allocator) !std.ArrayList(u8) {
-    var cp = std.ArrayList(u8).init(allocator);
-    try cp.appendSlice(str);
+pub inline fn copy(str: []const u8, allocator: mem.Allocator) ![]u8 {
+    const cp = mem.concat(allocator, u8, &[_][]const u8{str});
     return cp;
 }
-
